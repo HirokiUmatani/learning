e19777f64ce56eafb92735e52cce02550f65d28f
haruka <>
2020-07-31 18:22:05 +0900

    masterリセット

--- LeaningPush/Pods/Target Support Files/Pods-LeaningPush-LeaningPushUITests/Pods-LeaningPush-LeaningPushUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LeaningPush_LeaningPushUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LeaningPush_LeaningPushUITests
-@end
--- LeaningPush/Pods/Target Support Files/Pods-LeaningPushTests/Pods-LeaningPushTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LeaningPushTests : NSObject
-@end
-@implementation PodsDummy_Pods_LeaningPushTests
-@end
