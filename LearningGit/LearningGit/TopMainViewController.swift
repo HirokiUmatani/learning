83e43cff07407a1c461aecda3c8d5fc6a8b7a0da
Ueda-Masashi <mumunohanata@gmail.com>
2020-08-01 14:33:56 +0900

    TopViewController 追加

---
+++ LearningGit/LearningGit/TopMainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TopMainViewController.swift
+//  LearningGit
+//
+//  Created by むむ on 2020/08/01.
+//  Copyright © 2020 DEAUX. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopMainViewController: BaseViewController {
+}
+
+// MARK: - Life cycle
+extension TopMainViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopMainViewController {
+}
+
+// MARK: - method
+extension TopMainViewController {
+}
