49316e0ea9de71006b544c2fa423fe27b871b5c3
haruka <>
2020-08-01 14:34:53 +0900

    secondView作成開始

---
+++ LearningGit/LearningGit/Second/Controller/TopViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TopViewController.swift
+//  LearningGit
+//
+//  Created by haruka on 2020/08/01.
+//  Copyright © 2020 DEAUX. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: <#Super Class#> {
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+}
+
+// MARK: - method
+extension TopViewController {
+}
---
+++ LearningGit/LearningGit/Second/View/TopMainView.swift
@@ -0,0 +1,30 @@
+//
+//  TopMainView.swift
+//  LearningGit
+//
+//  Created by haruka on 2020/08/01.
+//  Copyright © 2020 DEAUX. All rights reserved.
+//
+
+import UIKit
+
+class TopMainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
