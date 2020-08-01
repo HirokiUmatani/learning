4d425986fee89c49bdbf7e38f08d62c43d51fcde
Ueda-Masashi <mumunohanata@gmail.com>
2020-08-01 16:21:27 +0900

    プロジェクトファイル２作成しました

---
+++ LearningGitTwo/LearningGitTwo/TopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TopViewController.swift
+//  LearningGitTwo
+//
+//  Created by むむ on 2020/08/01.
+//  Copyright © 2020 DEAUX. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
